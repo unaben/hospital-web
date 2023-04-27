401b03a63fee3acfa49d14030fe8946b6273eb43
unaben <unaben@yahoo.com>
2023-04-27 11:22:07 +0100

    [master] third commit

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
-import React from 'react'
+import React from "react";
 
 const Contacts = () => {
   return (
@@ -7,12 +7,12 @@ const Contacts = () => {
       <div className="container grid">
         <div className="contact-left">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2384.6268289831164!2d-6.214682984112116!3d53.29621947996855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x486709e0c9c80f8f%3A0x92f408d10f2277c2!2sREVO!5e0!3m2!1sen!2snp!4v1636264848776!5m2!1sen!2snp"
-            alt=""
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2430.067702449464!2d-1.9001395235013612!3d52.477909872050205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4870bc896c750ee5%3A0x9da3f5ae36c39f33!2sNew%20Street%20Station%2C%20East%20Mews%2C%20Birmingham%20B2%204PY!5e0!3m2!1sen!2suk!4v1682590781171!5m2!1sen!2suk"
             width="600"
             height="450"
             style={{ border: 0 }}
             loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
         <div className="contact-right text-white text-center bg-blue">
@@ -52,6 +52,6 @@ const Contacts = () => {
       </div>
     </section>
   );
-}
+};
 
-export default Contacts
+export default Contacts;
